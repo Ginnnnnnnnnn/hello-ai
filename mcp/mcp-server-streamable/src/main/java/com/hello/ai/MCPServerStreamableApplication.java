c654b0804c48cd43d04ee461aef19c6b716386b8
Gin <2440016523@qq.com>
2026-06-26 14:30:11 +0800

    MCP Streamable

---
+++ mcp/mcp-server-streamable/src/main/java/com/hello/ai/MCPServerStreamableApplication.java
@@ -0,0 +1,13 @@
+package com.hello.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCPServerStreamableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MCPServerStreamableApplication.class, args);
+    }
+
+}
---
+++ mcp/mcp-server-streamable/src/main/java/com/hello/ai/config/MCPConfig.java
@@ -0,0 +1,26 @@
+package com.hello.ai.config;
+
+import com.hello.ai.service.WeatherService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MCP
+ *
+ * @author Gin
+ * @since 2026-06-26
+ */
+@Configuration
+public class MCPConfig {
+
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        // 自动扫描 WeatherService 中带有 @Tool 注解的方法
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(weatherService)
+                .build();
+    }
+
+}
